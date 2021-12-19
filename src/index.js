ad43e0dd995d83d7200a131de4f174ad8bb81c1a
Andre Gama del Otero <andredelotero@gmail.com>
2021-12-19 20:27:59 -0300

    service worker added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import registerServiceWorker from "react-service-worker";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+registerServiceWorker.register();
