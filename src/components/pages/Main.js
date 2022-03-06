e438ff037f072682ff32edab3b04dda25ad16fa6
Andre Gama del Otero <andredelotero@gmail.com>
2022-03-06 17:21:53 -0300

    webpack chunk names

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,7 +1,13 @@
 import loadable from "@loadable/component";
-const DinamicHome = loadable(() => import("./Home"));
-const DinamicAbout = loadable(() => import("./About"));
-const DinamicContact = loadable(() => import("./Contact"));
+const DinamicHome = loadable(() =>
+  import(/* webpackChunkName: "home" */ "./Home")
+);
+const DinamicAbout = loadable(() =>
+  import(/* webpackChunkName: "about" */ "./About")
+);
+const DinamicContact = loadable(() =>
+  import(/* webpackChunkName: "contact" */ "./Contact")
+);
 
 const Main = () => (
   <main>
