9783cad86c8f5eeef91c600d8758d9aa9bced959
Andre Gama del Otero <andredelotero@gmail.com>
2022-03-06 18:01:42 -0300

    lazy imports

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,19 +1,27 @@
 import loadable from "@loadable/component";
+import { Suspense } from "react";
+import { lazy } from "@loadable/component";
+
 const DinamicHome = loadable(() =>
   import(/* webpackChunkName: "home" */ "./Home")
 );
-const DinamicAbout = loadable(() =>
+const DinamicAbout = lazy(() =>
   import(/* webpackChunkName: "about" */ "./About")
 );
-const DinamicContact = loadable(() =>
+
+const DinamicContact = lazy(() =>
   import(/* webpackChunkName: "contact" */ "./Contact")
 );
 
 const Main = () => (
   <main>
     <DinamicHome />
-    <DinamicAbout />
-    <DinamicContact />
+    <Suspense fallback={<div>Loading...</div>}>
+      <DinamicAbout />
+    </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DinamicContact />
+    </Suspense>
   </main>
 );
 
